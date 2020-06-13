115eae598d652ca71ef73e2cea99c2497074295b
Ale Bark <ale.bark_bruneri@hotmail.com>
2020-06-13 11:13:52 -0300

    loading items ok

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -12,16 +12,18 @@ Ale Bark Bruneri
 def main():
     html_handler = HtmlHandler(
         "https://startupbase.com.br/home/startups?q=&states=all&cities=all&segments=all&targets=all&phases=all&models=all&badges=all")
-    html = html_handler.get_html()
+    #html = html_handler.get_html()
 
     html_handler.maximize_window()
     html_handler.dismiss_popup('onesignal-popover-cancel-button')
     time.sleep(5)
 
-    html_handler.scroll_page_down(
-        '/html/body/app-root/ng-component/app-layout/div/div/div/div/div/app-layout-column/ng-component')
+    while not html_handler.is_visible_in_viewport_by_class_name('sbfooter'):
+        html_handler.scroll_page_down('/html/body', 5)
 
-    # soup = BeautifulSoup(html, 'html.parser')
+        # This will prevent fast scrolling which will cause the footer to visible
+        # for a very short period (causing the while loop to break) before the script load more items
+        time.sleep(2)
 
 
 if __name__ == '__main__':
--- web_scraper/models/HtmlHandler.py
+++ web_scraper/models/HtmlHandler.py
@@ -22,9 +22,24 @@ class HtmlHandler(object):
     def maximize_window(self):
         self.driver.maximize_window()
 
-    def dismiss_popup(self,id):
+    def dismiss_popup(self, id):
         self.driver.find_element_by_id(id).click()
 
-    def scroll_page_down(self, path):
-        component = self.driver.find_element_by_xpath('/html/body')
-        component.send_keys(Keys.PAGE_DOWN).perform()
+    def scroll_page_down(self, path, scroll_quantity):
+        component = self.driver.find_element_by_xpath(path)
+        for x in range(scroll_quantity):
+            component.send_keys(Keys.PAGE_DOWN)
+
+    def is_visible_in_viewport_by_class_name(self, classname):
+        element = self.driver.find_element_by_class_name(classname)
+        return self.driver.execute_script(
+            "var elem = arguments[0],                 " +
+            "  box = elem.getBoundingClientRect(),    " +
+            "  cx = box.left + box.width / 2,         " +
+            "  cy = box.top + box.height / 2,         " +
+            "  e = document.elementFromPoint(cx, cy); " +
+            "for (; e; e = e.parentElement) {         " +
+            "  if (e === elem)                        " +
+            "    return true;                         " +
+            "}                                        " +
+            "return false;                            ", element)
