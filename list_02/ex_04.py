d19da7fce27935f4a54fa45acb436b8f72ee2178
Ale Bark <ale.bark_bruneri@hotmail.com>
2020-06-17 21:18:47 -0300

    fix ex 03 and ex 04

--- list_02/ex_04.py
+++ list_02/ex_04.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
-# Algorithm that reads an integer and shows its square root
+
+# A pumpkin producer must check the classification of their products for later packaging and sale.
+# One of its customers buys only medium sized pumpkins (those with a diameter (d) in the range 15 cm ≤ d <20 cm).
+# Develop an algorithm that reads the diameter of a pumpkin and shows whether it is of the average type or not.
+# If it does not fit the classification, inform “product out of measure”
+
 import math
 
 
 def main():
 
-    user_input = input("Number: ")
+    user_input = input("Diameter: ")
 
     if user_input:
         if input_can_be_parsed(user_input) and input_is_a_positive_integer(user_input):
-            print("Square root:", math.sqrt(int(user_input)))
+            if(int(user_input) >= 15 and int(user_input) <= 20):
+                print("Correct product measure")
+            else:
+                print("Product out of measure")
             return True
     else:
         print("Empty input \n")
