2c57278b3d73c73a4615d85b6ef8c122dad71bd3
Ale Bark <ale.bark_bruneri@hotmail.com>
2020-06-24 21:04:08 -0300

    list 05 ex 01

---
+++ list_05/ex_01.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# An algorithm that shows the multiplication table from 1 to 10. (Ex: 1x1 = 1, 1x2 = 2, 10x10 = 100)
+
+def main():
+    for idx in range(1, 11):
+        print("1 * ", idx, '=', 1 * idx)
+
+
+if __name__ == '__main__':
+    main()
