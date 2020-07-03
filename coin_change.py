583f2703b2a780f8787e340c4238257e531766aa
Ale Bark <ale.bark_bruneri@hotmail.com>
2020-07-02 22:12:32 -0300

    changing loop conditions

--- coin_change.py
+++ coin_change.py
@@ -1,28 +1,19 @@
 # -*- coding: utf-8 -*-
 
 def main():
+    arr_moedas_existentes = [1, 50, 25, 10, 5]
+    arr_moedas_disponiveis = validar_moedas_disponiveis(arr_moedas_existentes)
 
-    arr_moedas_disponiveis = []
-    arr_moedas_disponiveis[0] = input("Qtde de moedas de um real: ")
-    arr_moedas_disponiveis[1] = input("Qtde de modedas de cinquenta centavos: ")
-    arr_moedas_disponiveis[2] = input("Qtde de moedas de vinte e cinco centavos: ")
-    arr_moedas_disponiveis[3] = input("Qtde de moedas de dez centavos: ")
-    arr_moedas_disponiveis[4] = input("Qtde de moedas de cinco centavos: ")
-
-    if validar_moedas_disponiveis(arr_moedas_disponiveis):
-
+    if arr_moedas_disponiveis:
         valor_conta = input("Valor da conta:")
         valor_pago = input("Valor pago:")
 
         condicoes_de_execucao = [
-            valor_conta,
-            valor_pago,
             validar_entrada_float(valor_conta),
             validar_entrada_float(valor_pago),
         ]
 
         if all(condicoes_de_execucao):
-
             valor_pago = float(valor_pago)
             valor_conta = float(valor_conta)
 
@@ -31,12 +22,11 @@ def main():
             else:
                 print("Valor pago insuficiente \n")
                 return False
-        print("Condições de execução não foram atendidas, abortando!")
+        print("Condições de execução não foram atendidas, reiniciando! \n")
         return True
 
 
 def calcular_troco(troco, arr_moedas_disponiveis):
-
     trocos_possieis = [100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10, 0.5, 0.1]
     changes_quantity = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -51,10 +41,18 @@ def calcular_troco(troco, arr_moedas_disponiveis):
     print(posible_changes, '-', changes_quantity)
 
 
-def validar_moedas_disponiveis(arr_moedas_disponiveis):
-    for moeda in arr_moedas_disponiveis:
-        return validar_entrada_float(moeda)
-    return True
+def validar_moedas_disponiveis(arr_moedas_existentes):
+    arr_moedas_disponiveis = []
+    for moeda in arr_moedas_existentes:
+        print("Qtde de moedas de " + str(moeda) + "", "real" if moeda == 1 else "centavos:")
+        moeda_da_vez = input()
+
+        if not validar_entrada_int(moeda_da_vez):
+            print("Valor inválido, reiniciando \n")
+            return False
+
+        arr_moedas_disponiveis.append(moeda_da_vez)
+    return arr_moedas_disponiveis
 
 
 def validar_entrada_float(input):
@@ -62,7 +60,14 @@ def validar_entrada_float(input):
         valor = float(input)
         return valor > 0
     except ValueError:
-        print("Entrada inválida")
+        return False
+
+
+def validar_entrada_int(input):
+    try:
+        valor = int(input)
+        return valor > 0
+    except ValueError:
         return False
 
 
