0273ebe51a2300c3d43ec1484317d5d9e725ffbf
Ale Bark <ale.bark_bruneri@hotmail.com>
2020-07-02 23:04:26 -0300

    returning change quantity

--- coin_change.py
+++ coin_change.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 def main():
-    arr_moedas_existentes = [1, 50, 25, 10, 5]
+    print("________________ \n")
+    arr_moedas_existentes = [1, 0.5, 0.25, 0.10, 0.5]
     arr_moedas_disponiveis = validar_moedas_disponiveis(arr_moedas_existentes)
 
     if arr_moedas_disponiveis:
@@ -18,6 +19,9 @@ def main():
             valor_conta = float(valor_conta)
 
             if valor_pago >= valor_conta:
+                if valor_pago - valor_conta == 0:
+                    print("Valor ok, não há necessidade de troco!")
+                    return False
                 return calcular_troco(valor_pago - valor_conta, arr_moedas_existentes, arr_moedas_disponiveis)
 
         print("Valores inválidos, reiniciando!")
@@ -25,12 +29,20 @@ def main():
 
 
 def calcular_troco(troco, arr_moedas_existentes, arr_moedas_disponiveis):
-    print("Troco fornecido:", troco, '\n')
+    print("Troco:", troco, '\n')
+    arr_moedas_troco = [0, 0, 0, 0, 0]
+    #troco = 10.21
+    #existente  [1, 0.5, 0.25, 0.10, 0.5]
+    #disponivel [3,  0,   0,    0,    1 ]
 
-    if troco != 0:
-        for x in range(len(arr_moedas_existentes)):
-            x = x
-    return True
+    for x in range(len(arr_moedas_existentes)):
+        quantidade_moeda = 0
+        while troco >= arr_moedas_existentes[x]:
+            if arr_moedas_disponiveis[x] >= 1:
+                troco = troco - arr_moedas_existentes[x]
+                quantidade_moeda += 1
+                arr_moedas_troco[x] = quantidade_moeda
+    print(arr_moedas_troco)
 
 
 def validar_moedas_disponiveis(arr_moedas_existentes):
@@ -43,7 +55,7 @@ def validar_moedas_disponiveis(arr_moedas_existentes):
             print("Valor inválido, reiniciando \n")
             return False
 
-        arr_moedas_disponiveis.append(moeda_da_vez)
+        arr_moedas_disponiveis.append(int(moeda_da_vez))
     return arr_moedas_disponiveis
 
 
@@ -58,7 +70,7 @@ def validar_entrada_float(input):
 def validar_entrada_int(input):
     try:
         valor = int(input)
-        return valor > 0
+        return valor >= 0
     except ValueError:
         return False
 
